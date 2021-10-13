86a0f9f7f17b7b5e2e776b24d5224c1408dc6a5f
Ismael Aquino <ismaellAquino@hotmail.com>
2021-10-13 17:39:54 -0300

    add usage to battery level info

--- lib/ui/screen/battery_info/battery_info.dart
+++ lib/ui/screen/battery_info/battery_info.dart
@@ -1,1 +1,2 @@
 export 'battery_info_screen.dart';
+export 'battery_info_presenter.dart';
--- lib/ui/screen/battery_info/battery_info_screen.dart
+++ lib/ui/screen/battery_info/battery_info_screen.dart
@@ -1,15 +1,63 @@
 import 'package:flutter/material.dart';
 
+import 'battery_info.dart';
+
 class BatteryInfoScreen extends StatefulWidget {
   static const String routeName = '/battery_info';
 
+  final BatteryInfoPresenter presenter;
+
+  BatteryInfoScreen({ Key? key, required this.presenter}) : super(key: key);
+
   @override
-  State<StatefulWidget> createState() => _BatteryInfoScreenState();
+  _BatteryInfoScreenState createState() => _BatteryInfoScreenState();
 }
 
-class _BatteryInfoScreenState extends State<StatefulWidget> {
+class _BatteryInfoScreenState extends State<BatteryInfoScreen> {
+  BatteryInfoPresenter get _presenter => widget.presenter;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _presenter.getBatteryLevel();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Theme(
+      data: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      child: Scaffold(
+        appBar: _buildAppBar(),
+        body: _buildBody(),
+      ),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      title: Text('Battery\'s Informations'),
+    );
+  }
+
+  Widget _buildBody() {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          StreamBuilder(
+            stream: _presenter.batteryLevel,
+            builder: (context, snapshot) {
+              print(snapshot.data);
+              return Text(
+                '${snapshot.data ?? 0}',
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
