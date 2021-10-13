2724d066c94ba4c9fca89333f10d6aed65e2a09a
Ismael Aquino <ismaellAquino@hotmail.com>
2021-10-13 11:54:19 -0300

    change router navigator to a Singleton

--- lib/ui/widgets/router_navigator.dart
+++ lib/ui/widgets/router_navigator.dart
@@ -3,7 +3,14 @@ import 'package:flutter/material.dart';
 import '../../main/factories/factories.dart';
 import '../screen/screen.dart';
 
+/// Singleton to manage all routes in application
 class RouterNavigator {
+  const RouterNavigator._();
+
+  static const RouterNavigator _instance = RouterNavigator._();
+
+  factory RouterNavigator.getInstance() => _instance;
+  
   static final navigatorKey = GlobalKey<NavigatorState>();
 
   static const String initialRoute = '/home_screen';
@@ -11,4 +18,8 @@ class RouterNavigator {
   static Map<String, Widget Function(BuildContext)> routes = {
     HomeScreen.routeName: makeHomeScreen,
   };
+
+  Future<T?> navigateTo<T>(String routeName) async {
+    return navigatorKey.currentState!.pushNamed<T>(routeName);
+  }
 }
